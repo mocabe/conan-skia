0008ede0983652abf994a9df916facc6ac3c27fa
mocabe <hisa.mocabe@gmail.com>
2025-09-21 22:39:34 +0900

    Add some includes for test

--- recipes/skia/all/test_package/src/example.cpp
+++ recipes/skia/all/test_package/src/example.cpp
@@ -1,6 +1,15 @@
 #include "include/core/SkCanvas.h"
 #include "include/core/SkSurface.h"
 
+#include "modules/skcms/skcms.h"
+#include "modules/skottie/include/Skottie.h"
+#include "modules/skparagraph/include/Paragraph.h"
+#include "modules/skresources/include/SkResources.h"
+#include "modules/sksg/include/SkSGScene.h"
+#include "modules/skshaper/include/SkShaper.h"
+#include "modules/skunicode/include/SkUnicode.h"
+#include "modules/svg/include/SkSVGDOM.h"
+
 int main()
 {
     sk_sp<SkSurface> surface = SkSurfaces::Raster(SkImageInfo::MakeN32Premul({1280, 720}));
